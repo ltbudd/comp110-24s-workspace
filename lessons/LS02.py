47407784dfd714a94f8c2f28a42102f766155bfa
Luke Buddendeck <ltbudd@unc.edu>
2024-01-17 16:25:54 -0500

    Created new LS file

---
+++ lessons/LS02.py
@@ -0,0 +1,8 @@
+__author__ = 730648114
+print(type(True))
+print(1_000 + 1_000_000)
+print(10 / 2)
+print("U" + "N" + "C")
+print("12345"[0])
+print("spooky season" [2 + 2])
+### print("spooky"[len("spooky")]) produces an unsightly error
