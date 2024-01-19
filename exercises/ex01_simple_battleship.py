2055c529ea0db85282667d39979dd6aa76dabf65
Luke Buddendeck <ltbudd@unc.edu>
2024-01-19 12:20:06 -0500

    Saving LS03, starting ex01

---
+++ exercises/ex01_simple_battleship.py
@@ -0,0 +1,2 @@
+"""EX01 - Simple Battleship - A cute step towards Battleship."""
+__author__ = "730648114"
---
+++ lessons/LS03.py
@@ -0,0 +1,10 @@
+__author__ = "730648114"
+
+from random import random
+from random import choice
+print(type(random()))
+print(type(choice("wacky")))
+
+print("comp110".isalpha())
+print("110".isdigit())
+print("comp110"[0].isalpha())
